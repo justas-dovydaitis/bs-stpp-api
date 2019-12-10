4c6b40899013089551563c6ff61faab148e4c923
Justas Dovydaitis <justuxlt1@gmail.com>
2019-12-10 23:31:58 +0200

    added origin for production

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ const cloudinaryStorage = require('multer-storage-cloudinary');
 const mongooseConn = require('./Utils/MongooseConnect');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
 const storage = cloudinaryStorage({
-    cloudinary: cloudinary,
-    folder: 'uploads',
-    allowedFormats: ['jpg', 'png'],
-    transformation: [{ width: 500, height: 500, crop: 'limit' }]
+  cloudinary: cloudinary,
+  folder: 'uploads',
+  allowedFormats: ['jpg', 'png'],
+  transformation: [{ width: 500, height: 500, crop: 'limit' }]
 });
 const parser = multer({ storage: storage });
 
@@ -34,25 +34,28 @@ const stage = require('./config')[environment];
 
 const routes = require('./Routes/index.js');
 
-const allowedOrigins = ['http://localhost:3000',
-                      'http://192.168.0.106:3000'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://192.168.0.106:3000',
+  'https://buildstuff.orangecactus.lt'
+];
 mongooseConn();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(parser.any());
 
 app.use(cors({
   credentials: true,
-  origin: function(origin, callback){
+  origin: function (origin, callback) {
     // allow requests with no origin 
     // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
       var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+        'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
@@ -60,13 +63,13 @@ app.use(cors({
 }));
 
 if (environment !== 'production') {
-    app.use(logger('dev'));
+  app.use(logger('dev'));
 }
 app.use('/uploads/', express.static('uploads'));
 app.use('/api/', routes(router));
 
 app.listen(`${stage.port}`, () => {
-    console.log(`Server now listening at localhost:${stage.port}`);
+  console.log(`Server now listening at localhost:${stage.port}`);
 });
 
 module.exports = app;
